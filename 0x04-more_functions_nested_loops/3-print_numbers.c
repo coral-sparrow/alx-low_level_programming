2645dd5761bbb96aee29e415ad329906c9a7ab27
mahmoud <mahmoud.shabban469@gmail.com>
2023-09-14 19:22:54 +0300

    ignore

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - returns if the char is Upper case
- *
- * Return: largest number
+ * print_numbers - prints the numbers, from 0 to 9,
+ * followed by a new line
  */
-
 void print_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
-		_puthcar('0' + i);
+	{
+		_putchar(i + '0');
+	}
+
 	_putchar('\n');
 }
