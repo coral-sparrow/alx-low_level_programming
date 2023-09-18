53d20de12f31c0fcd592ded00ca627b152600d2c
mahmoud <mahmoud.shabban469@gmail.com>
2023-09-19 00:32:06 +0300

    ignore

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -14,6 +14,4 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 
-#endifvoid reset_to_98(int *n);
-int _strlen(char *s);
-void swap_int(int *a, int *b);
+#endif
