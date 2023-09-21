c4317aae78cc7d9f05407aa00c1f850ad5f05a6d
mahmoud <mahmoud.shabban469@gmail.com>
2023-09-21 19:10:35 +0300

    ignore

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,32 +1,30 @@
 #include "main.h"
 
 /**
- * _strcat - prints a string to stdout
- * @dest: pointer to the string to print
- * @src: pointer to source
- * Return: pointer to the new string
+ * _strcat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to add
+ *
+ * Return: a pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	/* si, di: is the counter for the source/destination string */
-	int si, di = 0;
+	int i, j;
 
-	while (1)
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
 	{
-		if (dest[di] == '\0')
-		{
-			while (src[si] != '\0')
-			{
-				dest[si + di] = src[si];
-				si++;
-			}
-			dest[si + di] = '\0';
-			break;
-		}
-		else
-			di++;
+		dest[i] = src[j];
+		j++;
+		i++;
 	}
+
+	dest[i] = '\0';
 
 	return (dest);
 }
