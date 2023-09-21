47d51b485570b356882034689664d3e0541e97b0
mahmoud <mahmoud.shabban469@gmail.com>
2023-09-21 19:11:21 +0300

    ignore

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,30 @@
 #include "main.h"
 
 /**
- * _strncpy - prints a string to stdout
- * @dest: pointer to the string to print
- * @src: pointer to source
- * @n: number of bytes
- * Return: pointer to the new string
+ * _strncpy - copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to the resulting string
  */
-
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
 		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
