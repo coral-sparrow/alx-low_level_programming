b2c7aded7cfde56ddb0c1851b8d867f05a0d2d2d
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-03 01:18:34 +0300

    ignore

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the argument count
+ * @argv: array of cmd args
+ * Return: 0 for sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+
+}
