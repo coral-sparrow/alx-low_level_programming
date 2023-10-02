349d001faf20c5b87a66610cbc48518ea779ac7f
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-03 01:22:16 +0300

    ignore

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * @argc: the argument count
+ * @argv: array of cmd args
+ * Return: 0 for sucess
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+
+}
