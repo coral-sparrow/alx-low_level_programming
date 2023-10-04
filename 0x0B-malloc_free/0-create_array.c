01ee83c86a72efddfe1f5ab0bad95d4007ec2f1f
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-04 16:28:40 +0300

    ignore

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - checks for uppercase character
+ * @size: arr size
+ * @c: the character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+
+	if (size == 0)
+		return (NULL);
+
+	arr = malloc(sizeof(char) * size);
+	arr[0] = c;
+	if (arr == NULL)
+		return (NULL);
+	else
+		return (arr);
+}
