a711757c3396d5982dc7e2d84ab95647e7ee8a2b
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-05 00:45:18 +0300

    ignore

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * alloc_grid - checks for uppercase character
+ * @width: arr size
+ * @height: string 2
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i = 0, j = 0;
+
+	if (width && height)
+	{
+		arr = (int **) malloc(sizeof(int *) * width);
+		if (!arr)
+			return (NULL);
+
+		while (i < width)
+		{
+			j = 0;
+			arr[i] = (int *) malloc(sizeof(int) * height);
+			if (!arr[i])
+				return (NULL);
+			while (j < height)
+			{
+				arr[i][j] = 0;
+				j++;
+
+			}
+
+			i++;
+		}
+
+		return (arr);
+	}
+	else
+		return (NULL);
+}
+
+
