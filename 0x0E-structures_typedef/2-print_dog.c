80014839d63017fc6ec979968724cd5cd27bb47b
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-11 21:09:38 +0300

    ignore

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,15 +11,11 @@ void print_dog(struct dog *d)
 {
 	if (d)
 	{
-		if (d->name)
+		if (d->name != NULL)
 			printf("Name: %s\n", d->name);
 		else
 			printf("Name: (nil)\n");
-		if (d->age)
-			printf("Age: %.6f\n", d->age);
-		else
-			printf("Age: (nil)\n");
-		if (d->owner)
+		if (d->owner != NULL)
 			printf("Owner: %s\n", d->owner);
 		else
 			printf("Owner: (nil)\n");
