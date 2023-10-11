8555d4b6123a16eb856f507ef6de25add89376b0
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-11 20:04:07 +0300

    ignore

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - check the code for ALX School students.
+ * @d: dog
+ * @name: string
+ * @age: num
+ * @owner: ptr
+ *
+ * Return: Always 0.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
