7a40a12d8a681ab0c5a841ca6e930fa75a3cf334
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-11 22:41:37 +0300

    ignore

---
+++ 0x0E-structures_typedef/my-4-new_dog.c
@@ -0,0 +1,38 @@
+#include "dog.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - check the code for ALX School students.
+ * @name: dog
+ * @age: float
+ * @owner: char
+ *
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *p;
+
+	if (!name || !owner)
+		return (NULL);
+	p = malloc(sizeof(dog_t));
+
+	if (!p)
+		return (NULL);
+
+	p->name = strdup(name);
+	p->owner = strdup(owner);
+
+	if (!p->name || !p->owner)
+	{
+		free(p->name);
+		free(p->owner);
+		free(p);
+		return (NULL);
+	}
+
+	p->age = age;
+	return (p);
+}
