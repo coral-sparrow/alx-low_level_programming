0325fae7f9127904393b34633bfa62c2fdb4a598
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-24 23:32:46 +0300

    ignore

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - returns the largest of 3 numbers
+ * @h: pointer to list
+ * Return: largest number
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t *p;
+	size_t counter = 0;
+
+	printf("%d\n", h->n);
+	counter++;
+	p  = h->next;
+	while (p->next)
+	{
+		printf("%d\n", p->n);
+		p = p->next;
+		counter++;
+	}
+
+	printf("%d\n", p->n);
+	counter++;
+
+	return (counter);
+
+}
+
