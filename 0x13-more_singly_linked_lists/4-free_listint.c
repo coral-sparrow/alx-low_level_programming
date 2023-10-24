6feadb27fc627ba63756d4b0b217a2c8415bc855
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-25 00:38:42 +0300

    ignore

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint - returns the largest of 3 numbers
+ * @head: pointer to list
+ * Return: largest number
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *p;
+
+	p = head;
+
+	while (p->next)
+	{
+		p = p->next;
+		free(head);
+		head = p;
+	}
+
+	free(head);
+	p = NULL;
+
+}
+
