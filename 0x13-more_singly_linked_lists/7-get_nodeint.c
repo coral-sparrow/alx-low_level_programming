47d0469090087e280036e7473c143c2b2165c184
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-25 01:06:40 +0300

    ignore

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - returns the largest of 3 numbers
+ * @head: pointer to list
+ * @index: index
+ * Return: largest number
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *p;
+	unsigned int idx = 0;
+
+	if (head)
+	{
+		p = head;
+
+		while (p->next)
+		{
+			if (idx == index)
+				return (p);
+			p = p->next;
+			idx++;
+		}
+
+		return (NULL);
+	} else
+		return (NULL);
+}
+
