b8a4b84d60838e7e261e69d69cb95a827cbfdd9f
mahmoud <mahmoud.shabban469@gmail.com>
2023-10-30 18:48:08 +0200

    ignore

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,13 +12,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	ssize_t fd, w, i = 0;
-	int c;
+	/* int c;*/
 	char *buffer;
 
 	if (filename == NULL)
 		return (-1);
 
-	c = strcmp(text_content, "NULL");
+	/* c = strcmp(text_content, "NULL"); */
 
 	buffer = text_content;
 
@@ -26,12 +26,12 @@ int create_file(const char *filename, char *text_content)
 	if (fd < 0)
 		return (-1);
 
-	if (c)
+	if (text_content != NULL)
 	{
 		while (*buffer++)
 			i++;
 
-		w = write(fd, text_content, i + 1);
+		w = write(fd, text_content, i);
 		if (w < 0)
 			return (-1);
 	}
